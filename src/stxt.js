5c2d8fd1144a9f2f7dbd54c98e3f8a0eae41d8f2
Graydon Hoare <graydon@mozilla.com>
2013-12-14 21:48:52 -0800

    untabify

--- src/stxt.js
+++ src/stxt.js
@@ -2,29 +2,29 @@
 "use strict";
 
 function return_true() {
-	return true;
+    return true;
 }
 
 var Stxt = {
-	return_true: return_true,
+    return_true: return_true,
 
     sjcl: require('sjcl'),
-	curve25519: require('./curve25519.js'),
+    curve25519: require('./curve25519.js'),
     when: require('when'),
 
-	Assert: require('./assert.js'),
-	Config: require('./config.js'),
-	Fmt: require('./fmt.js'),
-	Graph: require('./graph.js'),
-	Group: require('./group.js'),
-	Hash: require('./hash.js'),
-	Key: require('./key.js'),
-	Msg: require('./msg.js'),
-	State: require('./state.js'),
-	Store: require('./store.js'),
-	Tag: require('./tag.js'),
-	Trace: require('./trace.js'),
-	Uletter: require('./uletter.js'),
+    Assert: require('./assert.js'),
+    Config: require('./config.js'),
+    Fmt: require('./fmt.js'),
+    Graph: require('./graph.js'),
+    Group: require('./group.js'),
+    Hash: require('./hash.js'),
+    Key: require('./key.js'),
+    Msg: require('./msg.js'),
+    State: require('./state.js'),
+    Store: require('./store.js'),
+    Tag: require('./tag.js'),
+    Trace: require('./trace.js'),
+    Uletter: require('./uletter.js'),
 };
 
 module.exports = Stxt;
